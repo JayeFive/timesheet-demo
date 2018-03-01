0fb27f79a6f773c1e865951d56363dedb96dbc3a
Johnny Olson <john.olson3@gmail.com>
2018-03-01 14:27:13 -0800

    intital commit

---
+++ timesheet-demo/Scripts/Application/index.js
@@ -0,0 +1,15 @@
+﻿function job(name) {
+    return {
+        name: ko.observable(name)
+    };
+}
+
+var viewModel = {
+    jobToAdd: "",
+    jobs: ko.observableArray([new job("johnny"), new job("anderson")]),
+
+    addJob: function () {
+        this.jobs.push(new job(this.jobToAdd));
+    },
+}
+ko.applyBindings(viewModel);
