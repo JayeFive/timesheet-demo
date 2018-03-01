dfc2cf948bf45a2942b01d2fe2dcdb1c90a56478
Johnny Olson <john.olson3@gmail.com>
2018-03-01 15:48:05 -0800

    addJob check to eliminate repeats

--- timesheet-demo/Scripts/Application/index.js
+++ timesheet-demo/Scripts/Application/index.js
@@ -9,7 +9,12 @@ var viewModel = {
     jobs: ko.observableArray([new job("johnny"), new job("anderson")]),
 
     addJob: function () {
-        this.jobs.push(new job(this.jobToAdd));
-    },
+        //this.jobs.push(new job(this.jobToAdd));
+        var objectToAdd = new job(this.jobToAdd);
+
+        if (this.jobs()[this.jobs().length - 1].name() !== objectToAdd.name()) {
+            this.jobs.push(new job(this.jobToAdd));
+        }
+    }
 }
 ko.applyBindings(viewModel);
