079c5b82aae20b86a0cf9bc8533f2502bd0178b0
Johnny Olson <john.olson3@gmail.com>
2018-03-05 17:46:05 -0800

    add form elements

--- timesheet-demo/Scripts/Application/index.js
+++ timesheet-demo/Scripts/Application/index.js
@@ -4,9 +4,27 @@
     };
 }
 
+function task(name) {
+    return {
+        name: ko.observable(name)
+    }
+}
+
+function hoursOptions() {
+    var optionsArray = [];
+
+    for (var i = 0.5; i <= 12; i += 0.5) {
+        optionsArray.push(i);
+    }
+
+    return optionsArray;
+}
+
 var viewModel = {
     jobToAdd: "",
     jobs: ko.observableArray([new job("johnny"), new job("anderson")]),
+    taskToAdd: "",
+    tasks: ko.observableArray([new task("siding"), new task("roofing")]),
 
     addJob: function () {
         var objectToAdd = new job(this.jobToAdd);
@@ -16,6 +34,18 @@ var viewModel = {
                 return;
         }
         this.jobs.push(new job(this.jobToAdd));
-    }
+    },
+
+    addTask: function () {
+        var objectToAdd = new task(this.taskToAdd);
+
+        for (var i = 0; i < this.tasks().length; i++) {
+            if (objectToAdd.name() === this.tasks()[i].name())
+                return;
+        }
+        this.tasks.push(new task(this.taskToAdd));
+    },
+
+
 }
 ko.applyBindings(viewModel);
